5911abde96d9becd2c03d601bccdd9a423c6f6d0
KshitizAgarwal21 <kshitizagarwal27@gmail.com>
2023-03-01 21:47:57 +0530

    dynamic rendering of buttons

--- src/Components/Common/Header/Header.js
+++ src/Components/Common/Header/Header.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./header.css";
 export default function Header() {
+  var [dynamic, setDynamic] = useState("none");
+
+  useEffect(() => {
+    if (localStorage.getItem("jwt") != null) {
+      setDynamic("inline-block");
+    }
+  }, [dynamic]);
+
   const navigate = useNavigate();
   const goback = () => {
     navigate(-1);
+  };
+  const login = () => {
+    localStorage.setItem("jwt", "cfxdtwcvwyd");
+    setDynamic("inline-block");
+  };
+  const logout = () => {
+    localStorage.removeItem("jwt");
+    setDynamic("none");
   };
   return (
     <div>
       <header>
         <span onClick={goback}>Back</span>
+        <span onClick={login}>Login</span>
+        <span onClick={logout}>Logout</span>
         <ul>
           <Link to="/">
             <li>Home</li>
@@ -21,7 +39,7 @@ export default function Header() {
             <li>Services</li>
           </Link>
           <Link to="/myorders">
-            <li>My Orders</li>
+            <li style={{ display: dynamic }}>My Orders</li>
           </Link>
         </ul>
       </header>
